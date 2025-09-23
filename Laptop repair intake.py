5164384f32be96f123301587e9ee59f2b07fe8b7
emelynineza-hub <emelynineza@gmail.com>
2025-09-23 10:26:04 +0200

    Delete Laptop repair intake.py

--- Laptop repair intake.py
+++
@@ -1,6 +0,0 @@
-intake_counts = [10, 15, 7, 20, 12, 9, 14]
-total_intake = sum(intake_counts)
-average_intake = total_intake / len(intake_counts)
-min_intake = min(intake_counts)
-max_intake = max(intake_counts)
-
